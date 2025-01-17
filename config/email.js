0c95e8897051f58daea450d4979cd5de643badf1
Komron Mamataliyev <komronmamataliyev@gmaill.com>
2025-01-17 16:58:40 +0500

    ok

--- config/email.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    service: "gmail",
-    auth: {
-        user: "komronmamataliyev@gmail.com", // Gmail manzilingiz
-        pass: "990664417" // Google yaratgan maxsus parol
-    }
-};
